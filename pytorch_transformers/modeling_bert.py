5432ecce654eceb66271a54669a40dfd22f298c9
zerohd4869 <1228868719@qq.com>
2020-07-28 21:50:04 +0800

    update modeling_bert.py

--- pytorch_transformers/modeling_bert.py
+++ pytorch_transformers/modeling_bert.py
@@ -1041,6 +1041,40 @@ class BertForSequenceClassification(BertPreTrainedModel):
         return outputs
 
 
+class LabelSmoothingLoss(nn.Module):
+    """This is label smoothing loss function.
+    """
+
+    def __init__(self, classes, smoothing=0.0, dim=-1):
+        super(LabelSmoothingLoss, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.cls = classes
+        self.dim = dim
+
+    def forward(self, pred, target, reward):
+        pred = pred.log_softmax(dim=self.dim)
+        true_dist = smooth_one_hot(target, self.cls, self.smoothing)
+        return torch.mean(reward.float() * torch.sum(-true_dist * pred, dim=self.dim))
+
+
+@torch.no_grad()
+def smooth_one_hot(true_labels: torch.Tensor, classes: int, smoothing=0.0):
+    """
+    if smoothing == 0, it's one-hot method
+    if 0 < smoothing < 1, it's smooth method
+    Warning: This function has no grad.
+    """
+    # assert 0 <= smoothing < 1
+    confidence = 1.0 - smoothing
+    label_shape = torch.Size((true_labels.size(0), classes))
+
+    smooth_label = torch.empty(size=label_shape, device=true_labels.device)
+    smooth_label.fill_(smoothing / (classes - 1))
+    smooth_label.scatter_(1, true_labels.data.unsqueeze(1), confidence)
+    return smooth_label
+
+
 @add_start_docstrings("""Bert Model with a multiple choice classification head on top (a linear layer on top of
     the pooled output and a softmax) e.g. for RocStories/SWAG tasks. """,
                       BERT_START_DOCSTRING)
